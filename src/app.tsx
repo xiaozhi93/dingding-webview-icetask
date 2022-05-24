b5159d8cef7c9cc30323d4394501a29fcb977fb6
jzy01309237 <jzy01309237@alibaba-inc.com>
2022-05-24 15:08:21 +0800

    移除微前端代码

--- src/app.tsx
+++ src/app.tsx
@@ -19,43 +19,43 @@ const appConfig: IAppConfig = {
   router: {
     type: 'browser',
   },
-  icestark: {
-    Layout: FrameworkLayout,
-    getApps: async () => {
-      const apps = [{
-        path: '/seller',
-        title: '商家平台',
-        loadScriptMode: 'import',
-        sandbox: true,
-        // React app demo: https://github.com/ice-lab/react-materials/tree/master/scaffolds/icestark-child
-        entry: 'http://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-seller-ice-vite/index.html',
-      }, {
-        path: '/waiter',
-        title: '小二平台',
-        loadScriptMode: 'import',
-        sandbox: true,
-        entry: 'http://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-vue3-vite/index.html',
-      }, {
-        path: '/angular',
-        title: 'Angular',
-        sandbox: true,
-        // Angular app demo: https://github.com/ice-lab/icestark-child-apps/tree/master/child-common-angular
-        entry: 'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-common-angular/index.html',
-      },
-      // , {
-      //   path: '/ice',
-      //   title: 'ice',
-      //   sandbox: true,
-      //   loadScriptMode: 'fetch',
-      //   entry: 'http://localhost:3333', // icetask-child
-      // }
-      ];
-      return apps;
-    },
-    appRouter: {
-      LoadingComponent: PageLoading,
-    },
-  },
+  // icestark: {
+  //   Layout: FrameworkLayout,
+  //   getApps: async () => {
+  //     const apps = [{
+  //       path: '/seller',
+  //       title: '商家平台',
+  //       loadScriptMode: 'import',
+  //       sandbox: true,
+  //       // React app demo: https://github.com/ice-lab/react-materials/tree/master/scaffolds/icestark-child
+  //       entry: 'http://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-seller-ice-vite/index.html',
+  //     }, {
+  //       path: '/waiter',
+  //       title: '小二平台',
+  //       loadScriptMode: 'import',
+  //       sandbox: true,
+  //       entry: 'http://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-vue3-vite/index.html',
+  //     }, {
+  //       path: '/angular',
+  //       title: 'Angular',
+  //       sandbox: true,
+  //       // Angular app demo: https://github.com/ice-lab/icestark-child-apps/tree/master/child-common-angular
+  //       entry: 'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-common-angular/index.html',
+  //     },
+  //     // , {
+  //     //   path: '/ice',
+  //     //   title: 'ice',
+  //     //   sandbox: true,
+  //     //   loadScriptMode: 'fetch',
+  //     //   entry: 'http://localhost:3333', // icetask-child
+  //     // }
+  //     ];
+  //     return apps;
+  //   },
+  //   appRouter: {
+  //     LoadingComponent: PageLoading,
+  //   },
+  // },
 };
 
 runApp(appConfig);
