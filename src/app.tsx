c4279b9b24e9a01a519d3e7196a86d687e9992f1
jzy01309237 <jzy01309237@alibaba-inc.com>
2022-05-24 15:31:28 +0800

    其他

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @iceworks/best-practices/no-http-url */
 import { runApp, IAppConfig } from 'ice';
 import { ConfigProvider } from '@alifd/next';
-// import PageLoading from '@/components/PageLoading';
+import PageLoading from '@/components/PageLoading';
 // import FrameworkLayout from '@/layouts/FrameworkLayout';
 import UserLayout from '@/layouts/UserLayout';
 
@@ -22,40 +22,28 @@ const appConfig: IAppConfig = {
   },
   icestark: {
     Layout: UserLayout,
-    // getApps: async () => {
-    //   const apps = [{
-    //     path: '/seller',
-    //     title: '商家平台',
-    //     loadScriptMode: 'import',
-    //     sandbox: true,
-    //     // React app demo: https://github.com/ice-lab/react-materials/tree/master/scaffolds/icestark-child
-    //     entry: 'http://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-seller-ice-vite/index.html',
-    //   }, {
-    //     path: '/waiter',
-    //     title: '小二平台',
-    //     loadScriptMode: 'import',
-    //     sandbox: true,
-    //     entry: 'http://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-vue3-vite/index.html',
-    //   }, {
-    //     path: '/angular',
-    //     title: 'Angular',
-    //     sandbox: true,
-    //     // Angular app demo: https://github.com/ice-lab/icestark-child-apps/tree/master/child-common-angular
-    //     entry: 'https://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-common-angular/index.html',
-    //   },
-    //   // , {
-    //   //   path: '/ice',
-    //   //   title: 'ice',
-    //   //   sandbox: true,
-    //   //   loadScriptMode: 'fetch',
-    //   //   entry: 'http://localhost:3333', // icetask-child
-    //   // }
-    //   ];
-    //   return apps;
-    // },
-    // appRouter: {
-    //   LoadingComponent: PageLoading,
-    // },
+    getApps: async () => {
+      const apps = [{
+        path: '/seller',
+        title: '商家平台',
+        loadScriptMode: 'import',
+        sandbox: true,
+        // React app demo: https://github.com/ice-lab/react-materials/tree/master/scaffolds/icestark-child
+        entry: 'http://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-seller-ice-vite/index.html',
+      }, {
+        path: '/waiter',
+        title: '小二平台',
+        loadScriptMode: 'import',
+        sandbox: true,
+        entry: 'http://iceworks.oss-cn-hangzhou.aliyuncs.com/icestark/child-vue3-vite/index.html',
+      },
+
+      ];
+      return apps;
+    },
+    appRouter: {
+      LoadingComponent: PageLoading,
+    },
   },
 };
 
